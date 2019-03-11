e84139671016e228559ac6d6577e195360ca8a60
Maxim Lykov (friseon) <frise.on@gmail.com>
2019-03-11 20:51:00 +0300

    proportions

--- src/chart.js
+++ src/chart.js
@@ -7,11 +7,24 @@ import {
 
 export default class Chart {
     constructor(params) {
-        const canvas = document.getElementById(params.idCanvas);
+        const chartContainer = document.querySelector('.chart');
+        const canvas = document.createElement('canvas');
+        canvas.id = params.idCanvas;
+
+        const canvasSearch = document.createElement('canvas');
+        canvasSearch.id = "canvas-search";
+
+        chartContainer.appendChild(canvas);
+        chartContainer.appendChild(canvasSearch);
+
         this.width = canvas.width = params.width || 800;
         this.height = canvas.height = params.height || 600;
 
+        this.searchWidth = canvasSearch.width = canvas.width;
+        this.searchHeight = canvasSearch.height = 100;
+
         this.ctx = canvas.getContext("2d");
+        this.ctxSearch = canvasSearch.getContext("2d");
 
         this.colors = {
             bg: {
@@ -21,6 +34,10 @@ export default class Chart {
         };
 
         this._prepareChartParams(params.data);
+    }
+
+    init() {
+
     }
 
     /**
@@ -36,6 +53,10 @@ export default class Chart {
 
         this.lines = preparedData;
         this.chartParams = {
+            padding: {
+                top: 10,
+                bottom: 100
+            },
             deviders,
             dates,
             max,
@@ -47,9 +68,10 @@ export default class Chart {
      * Отрисовка графика
      */
     draw() {
-        this._drawBackground();
+        this._drawBackground(this.ctx);
         this._drawCharts();
         this._drawDividers();
+        this._drawSearch();
     }
 
     /**
@@ -57,8 +79,34 @@ export default class Chart {
      * 
      * @param {Number} val – значение в данных
      */
-    _prepareValue(val) {
-        return this.height - this.height / this.chartParams.max * val;
+    _prepareValue(val, height) {
+        const p = height - this.chartParams.padding.top - this.chartParams.padding.bottom;
+
+        return height - p / this.chartParams.max * val - this.chartParams.padding.bottom;
+    }
+
+    _prepareSearchValue(val, height) {
+        return height - height / this.chartParams.max * val;
+    }
+
+    _drawSearch() {
+        this._drawBackground(this.ctxSearch);
+
+        this.lines.forEach(line => {
+            line.data.forEach((y, index, arr) => {
+                const preparedValue = this._prepareSearchValue(y, this.searchHeight);
+
+                if (index === 0) {
+                    this._startLine(this.ctxSearch, 0, preparedValue, line.color);
+                } else {
+                    this._drawLine(this.ctxSearch, this.chartParams.step * index, preparedValue);
+
+                    if (index === arr.length - 1) {
+                        this._endDraw(this.ctxSearch);
+                    }
+                }
+            })
+        });
     }
 
     /**
@@ -67,15 +115,15 @@ export default class Chart {
     _drawCharts() {
         this.lines.forEach(line => {
             line.data.forEach((y, index, arr) => {
-                const preparedValue = this._prepareValue(y);
+                const preparedValue = this._prepareValue(y, this.height);
 
                 if (index === 0) {
-                    this._startLine(0, preparedValue, line.color);
+                    this._startLine(this.ctx, 0, preparedValue, line.color);
                 } else {
-                    this._drawLine(this.chartParams.step * index, preparedValue);
+                    this._drawLine(this.ctx, this.chartParams.step * index, preparedValue);
 
                     if (index === arr.length - 1) {
-                        this._endDraw();
+                        this._endDraw(this.ctx);
                     }
                 }
             })
@@ -84,51 +132,51 @@ export default class Chart {
 
     _drawDividers() {
         this.chartParams.deviders.forEach(devider => {
-            const preparedValue = this._prepareValue(devider);
+            const preparedValue = this._prepareValue(devider, this.height);
 
-            this._startLine(0, preparedValue, "#f5f5f5", 0.5);
+            this._startLine(this.ctx, 0, preparedValue, "#f5f5f5", 0.5);
             this.ctx.font = "12px Arial";
             this.ctx.fillStyle = "#f5f5f5";
             this.ctx.fillText(devider, 5, preparedValue - 5);
-            this._drawLine(this.width, preparedValue);
-            this._endDraw();
+            this._drawLine(this.ctx, this.width, preparedValue);
+            this._endDraw(this.ctx);
         });
     }
 
     /**
      * Подложка для графика
      */
-    _drawBackground() {
-        this._startDraw();
-        this.ctx.fillStyle = this.colors.bg.night;
-        this.ctx.fillRect(0, 0, this.width, this.height);
-        this._endDraw();
+    _drawBackground(ctx) {
+        this._startDraw(ctx);
+        ctx.fillStyle = this.colors.bg.night;
+        ctx.fillRect(0, 0, this.width, this.height);
+        this._endDraw(ctx);
     }
 
-    _startLine(startX, startY, color, lineWidth = 1) {
-        this._startDraw();
-        this.ctx.strokeStyle = color;
-        this.ctx.lineWidth = lineWidth;
-        this.ctx.beginPath();
-        this.ctx.moveTo(startX, startY);
+    _startLine(ctx, startX, startY, color, lineWidth = 1) {
+        this._startDraw(ctx);
+        ctx.strokeStyle = color;
+        ctx.lineWidth = lineWidth;
+        ctx.beginPath();
+        ctx.moveTo(startX, startY);
     }
 
-    _drawLine(endX, endY) {
-        this.ctx.lineTo(endX, endY);
-        this.ctx.stroke();
+    _drawLine(ctx, endX, endY) {
+        ctx.lineTo(endX, endY);
+        ctx.stroke();
     }
 
     /**
      * Начало рисования самостоятельной части
      */
-    _startDraw() {
-        this.ctx.save();
+    _startDraw(ctx) {
+        ctx.save();
     }
 
     /**
      * Окончания рисования самостоятельной части
      */
-    _endDraw() {
-        this.ctx.restore();
+    _endDraw(ctx) {
+        ctx.restore();
     }
 };
